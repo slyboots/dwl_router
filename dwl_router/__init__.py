ef5b77ab17b43fb2cbb8a02de2670a0c1f4f815b
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2018-04-27 16:17:21 -0500

    updated main app

--- dwl_router/__init__.py
+++ dwl_router/__init__.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/rgleads'):
+def log_leadrouter_lead():
+    '''log information from lead router'''
+    app.logger.info(request.get_json())
+    return 'Lead Received'
