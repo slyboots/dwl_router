8fdccf432e54c1d37f7f654ec6a9a901ba0f4e2f
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2018-05-02 17:24:32 -0500

    app uses factory method for configuration

--- dwl_router/__init__.py
+++ dwl_router/__init__.py
@@ -1,13 +1,36 @@
+import os
 import logging
 from flask import Flask, request
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        MODE=os.getenv('FLASK_ENV'),
+    )
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-@app.route('/rgleads', methods=['GET', 'POST'])
-def log_leadrouter_lead():
-    '''log information from lead router'''
-    app.logger.info(request.get_json())
-    return 'Lead Received'
+    # a simple page that says hello
+    @app.route('/')
+    def hello():
+        return 'Hello, World!'
+
+
+    @app.route('/rgleads', methods=['GET', 'POST'])
+    def log_leadrouter_lead():
+        '''log information from lead router'''
+        app.logger.info(request.get_json())
+        return 'Lead Received'
+
+
+    return app
