4246b703fef2cee5aafcdedd0963117262e56985
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2018-05-02 19:29:31 -0500

    added logging import

--- application.py
+++ application.py
@@ -1,3 +1,9 @@
+import logging
 from dwl_router import create_app
+FORMAT = '%(asctime)-15s [%(levelname)s] %(message)s'
+DATE_FMT = '%m/%d/%Y %H:%M:%S'
+
+loglevel = logging.DEBUG if config.verbose else logging.INFO
+logging.basicConfig(format=FORMAT, datefmt=DATE_FMT, level=loglevel)
 
 application = create_app()
